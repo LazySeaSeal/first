e048da3eccdde55e17109a056eab0153c2bdb515
LazySeaSeal <achrafjlassi2001@gmail.com>
2024-01-20 19:01:06 +0100

    we used dependency injection for db in Product Controller + setget products with and without id (with a good practice)

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
 
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -7,16 +10,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of pro";
+            _context =context; //bech iwali aana acess il db context ..
+        }
+       
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() // we added async(good practice) + instead of the whole thread waiting for a response from db , it simply taaml another task (another thread to do the job without stoping everything)
+        {
+            var products= await _context.Products.ToListAsync(); // kona najmy naamluha fard ligne just as get bil id
+            return products;
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return $"this will be a product ++ {id}";
+            return await _context.Products.FindAsync(id); // it will look using the primary key !
         }
 
     }
